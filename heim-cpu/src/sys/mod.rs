f8701b3f8780ac5bac64ba67d93b19606a57d4b0
kt <ktullavik@gmail.com>
2020-11-05 18:00:46 +0100

    Recognize freebsd

--- heim-cpu/src/sys/mod.rs
+++ heim-cpu/src/sys/mod.rs
@@ -12,10 +12,14 @@ cfg_if::cfg_if! {
         mod windows;
 
         pub use self::windows::*;
-    }else if #[cfg(target_os = "macos")] {
+    } else if #[cfg(target_os = "macos")] {
         mod macos;
 
         pub use self::macos::*;
+    } else if #[cfg(target_os = "freebsd")] {
+        mod freebsd;
+
+        pub use self::freebsd::*;
     } else {
         compile_error!("Unsupported target OS");
     }
